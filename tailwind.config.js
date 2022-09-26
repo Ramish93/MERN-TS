5552360a10721c3032aba0cf9d01a8ddfd670191
Ramish Hassan <ramishshah93@gmail.com>
2022-09-26 23:43:10 +0200

    added _document file for font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./pages/**/*.{js, ts, jsx, tsx}', './components/**/*.{js, ts, tsx, jsx}'],
   theme: {
     extend: {},
   },
