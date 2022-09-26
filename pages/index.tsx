71c8708c0849e7e42e946f3fefbfaedecc019b73
Ramish Hassan <ramishshah93@gmail.com>
2022-09-27 00:17:07 +0200

    header component done showing image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from 'next'
+import Header from './components/Header/Header'
 
 
 const Home: NextPage = () => {
   return (
-    <main >
-      hello world
+    <main className='relative h-screen overflow-y-scroll ' >
+      <Header />
      </main>
   )
 }
