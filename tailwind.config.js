6950cb684d5ae20d684f23be5664cadb38714029
Ramish Hassan <ramishshah93@gmail.com>
2022-09-26 23:55:32 +0200

    installed tanstack-react query and setup app.tsx file with it

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./pages/**/*.{js, ts, jsx, tsx}', './components/**/*.{js, ts, tsx, jsx}'],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,tsx,jsx}'],
   theme: {
     extend: {},
     fontFamily: {
