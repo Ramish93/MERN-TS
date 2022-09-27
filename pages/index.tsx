cb48ce2eb7b10e68d417255026e63e6ed5fcd45d
Ramish Hassan <ramishshah93@gmail.com>
2022-09-27 22:54:55 +0200

    fetch movies function fetching movies as expected

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,7 @@
+import React,{useState} from 'react'
 import type { NextPage } from 'next'
+
+import { useFetchMovies } from '../api/fetchHooks'
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
 import Spinner from './components/Spinner/Spinner'
@@ -7,6 +10,13 @@ import Grid from './components/Grid/Grid'
 
 
 const Home: NextPage = () => {
+  const [query, setQuery] = useState('')
+
+  const {data, fetchNextPage, isLoading, isFetching, error} = useFetchMovies(query)
+
+  console.log(data);
+  
+
   return (
     <main className='relative h-screen overflow-y-scroll ' >
       <Header />
