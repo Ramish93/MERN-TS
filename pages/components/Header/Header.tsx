a0e3822d85d0f172e1861f92fc78edd149febffc
Ramish Hassan <ramishshah93@gmail.com>
2022-09-27 19:50:17 +0200

    components structure layed out in components folder

--- pages/components/Header/Header.tsx
+++ pages/components/Header/Header.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 
 const Header = () => (
     <div className='sticky flex top-0 z-40 w-full h-24 bg-zinc-900'>
-      <div className='flex justify-between w-full h-full max-w-7xl m-auto px-4'>
+      <div className='flex justify-between w-full h-full max-w-7xl m-auto px-2'>
         <Link href='/'>
           <div className='flex items-center cursor-pointer'>
             <div className='invisible md:visible'>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
 import type { NextPage } from 'next'
 import Header from './components/Header/Header'
+import Hero from './components/Hero/Hero'
+import Spinner from './components/Spinner/Spinner'
+import Card from './components/Card/Card'
+import Grid from './components/Grid/Grid'
 
 
 const Home: NextPage = () => {
   return (
     <main className='relative h-screen overflow-y-scroll ' >
       <Header />
+      <Hero />
+      <Grid />
+      <Card />
+      <Spinner />
      </main>
   )
 }
