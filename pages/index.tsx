0a04a7bd564641aa8de21b7563e59d4f6db8fd4a
Ramish Hassan <ramishshah93@gmail.com>
2022-09-28 21:02:24 +0200

    hero image displaying on search for a movie, hero component complete

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import React,{useState} from 'react'
 import type { NextPage } from 'next'
 
+import { IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE } from '../config'
 import { useFetchMovies } from '../api/fetchHooks'
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
@@ -20,7 +21,13 @@ const Home: NextPage = () => {
   return (
     <main className='relative h-screen overflow-y-scroll ' >
       <Header setQuery={setQuery} />
-      <Hero />
+      {query && data && data.pages ? (
+
+        <Hero imgUrl={data?.pages[0].results[0]?.backdrop_path ? IMAGE_BASE_URL + BACKDROP_SIZE + data.pages[0].results[0].backdrop_path : '/no_image.jgp'} 
+        title={data?.pages[0].results[0]?.title}
+        text={data?.pages[0].results[0]?.overview}
+         />
+      ): null}
       <Grid />
       <Card />
       <Spinner />
