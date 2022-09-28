ffa6a152162c08b5de18e171252c6523c981b029
Ramish Hassan <ramishshah93@gmail.com>
2022-09-28 22:22:07 +0200

    created thumb compoannet for the thumbnail image

---
+++ pages/components/Thumb/Thumb.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Image from 'next/image'
+
+type Props ={
+    imgUrl:string;
+}
+
+const Thumb = ({imgUrl}: Props) => 
+   (
+    <Image
+        placeholder='blur'
+        blurDataURL='/placeholder.jpg'
+        className='rounded-lg '
+        layout='fill'
+        objectFit='cover'
+        src={imgUrl}
+        alt='movie image thumbnail'
+    />
+  )
+
+
+export default Thumb
