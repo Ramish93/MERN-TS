f1a2231b17dbdfe33d40e3878a070e6254d90453
Ramish Hassan <ramishshah93@gmail.com>
2022-09-29 23:09:56 +0200

    MovieInfo component almost complete

--- components/MovieInfo/MovieInfo.tsx
+++ components/MovieInfo/MovieInfo.tsx
@@ -1,8 +1,38 @@
+import Image from "next/image"
+
+import { calcTime, convertMoney } from "../../helpers"
+import Thumb from "../Thumb/Thumb"
+import Pill from "../Pill/Pill"
+import { Crew } from "../../api/types"
 
 
-const MovieInfo = () => 
+type Props = {
+    thumbUrl: string;
+    backgroundImgUrl: string;
+    title: string;
+    year: string;
+    summary: string;
+    rating: number;
+    directors: Crew[];
+    time: number;
+    budget: number;
+    revenue: number;
+}
+const MovieInfo = ({thumbUrl, backgroundImgUrl, title, year, summary, rating, directors, time, budget, revenue}) => 
  (
-    <div>MovieInfo</div>
+    <div className="relative w-full h-auto p-4">
+        <div className="relative h-full min-h-128 flex flex-col md:flex-row max-w-7xl p-4 m-auto z-10 rounded-xl bg-zinc-800 bg-opacity-90">
+            <div className="relative w-full h-96 md:h-auto md:w-1/3">
+                <Thumb imgUrl={thumbUrl} />
+                <div className="absolute top-4 left-4 rounded-full bg-white  w-10 h-10 flex justify-center items-center text-black text-sm font-bold">
+                    {rating}
+                </div>
+            </div>
+            <div className="text-white px-0 py-4 md:py-0 text-center md:text-left md:px-8 w-full md:w-2/3">
+                <h2 className="text-2xl md:text-4xl font-bold pb-4">{title}, and year: ({year})</h2>
+            </div>
+        </div>
+    </div>
   )
 
 
