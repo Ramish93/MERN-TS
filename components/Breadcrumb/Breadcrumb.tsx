2c605620183180db27258f003fd86cb5467c2885
Ramish Hassan <ramishshah93@gmail.com>
2022-09-29 20:40:46 +0200

    breadcrump compoenet for movie title, details complete

--- components/Breadcrumb/Breadcrumb.tsx
+++ components/Breadcrumb/Breadcrumb.tsx
@@ -1,8 +1,21 @@
+import Link from "next/link"
 
+type Props = {
+  title: string,
 
-const BreadCrumb = () => 
+}
+
+const BreadCrumb = ({title}: Props) => 
  (
-    <div>BreadCrumb</div>
+    <div className="bg-zinc-800">
+      <div className="flex items-center max-w-7xl m-auto p-4 text-white text-lg">
+        <Link href={'/'}>
+          <span className="hover:opacity-80 cursor-pointer duration-300">Home</span>
+        </Link>
+          <span className="blcok px-2 ">|</span>
+          <span className="font-bold truncate">{title}</span>
+      </div>
+    </div>
   )
 
 
